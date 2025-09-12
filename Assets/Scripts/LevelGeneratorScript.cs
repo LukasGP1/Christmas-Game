e8e32197a73e79400c9ba1e409adf4be5bd07440
Lulkas <lukas.hilger@icloud.com>
2025-09-12 23:35:46 +0200

    Add Window to LevelGeneration
    Add the window in the LevelGeneratorScript as an Accesorie to a floor, just like the fire and the horse.

--- Assets/Scripts/LevelGeneratorScript.cs
+++ Assets/Scripts/LevelGeneratorScript.cs
@@ -9,6 +9,7 @@ public class LevelGeneratorScript : MonoBehaviour
     [SerializeField] public GameObject fire;
     [SerializeField] public GameObject water;
     [SerializeField] public GameObject horse;
+    [SerializeField] public GameObject window;
 
     public bool RandomChance(System.Random random, float chance)
     {
@@ -54,14 +55,17 @@ public class LevelGeneratorScript : MonoBehaviour
             Transform instantiatedFartLoadingZone = Instantiate(fartLoadingZone, fartRect.GetCenter(), new Quaternion(), parent).transform;
             instantiatedFartLoadingZone.localScale = fartRect.GetDimensions();
 
-            // If it's not the starting tile or the end tile, try to add accessories
+            // If it's not the starting tile or the end tile, try to add an accessorie
             if (i != 0 && i != tileCount + 1)
             {
                 // 20% chance to add a fire
                 if (RandomChance(random, 20)) Instantiate(fire, new Vector3(rect.GetCenter().x, rect.GetTopY(), 0), new Quaternion(), parent);
 
                 // If no fire is added, there is a 20% chance to add a horse
-                else if (RandomChance(random, 20)) Instantiate(horse, new Vector3(rect.GetCenter().x, rect.GetTopY() + 1, 0), new Quaternion(), parent).GetComponent<HorseScript>().SetFloorWidth(rect.GetWidth() - 0.5f);
+                else if (RandomChance(random, 25)) Instantiate(horse, new Vector3(rect.GetCenter().x, rect.GetTopY() + 1, 0), new Quaternion(), parent).GetComponent<HorseScript>().SetFloorWidth(rect.GetWidth() - 0.5f);
+
+                // If there is neither a horse nor a fire added, there is a 20% chance to add a window
+                else if (RandomChance(random, 33)) Instantiate(window, new Vector3(rect.GetCenter().x, rect.GetTopY() + 5, 0), new Quaternion(), parent);
             }
 
             // Instantiate the last tile (you need to adjust the height because of its different shape)
